6ef7a4d24543da95bbc12fbaeda93ebb0a44b3a4
Kayla Lin <klin23@u.rochester.edu>
2023-09-27 13:22:12 -0400

    fix: removed browser router url start

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { theme } from './setup/Theme/theme';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import ProjectContextProvider from './feature/projectContext';
-import PageContainer from './common/components/PageContainer';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <ProjectContextProvider>
         <ParallaxProvider>
-          <BrowserRouter basename="/kayla-lin-portfolio">
+          <BrowserRouter>
             {/* <LoadingScreen /> */}
             <Container maxW="6xl">
               <AppRouter />
